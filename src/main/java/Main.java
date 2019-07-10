263aa0657c04f313c12caa812e8eb90c235f089d
TexHaPb85 <16fi.m.yevtushenko@std.npu.edu.ua>
2019-07-10 23:09:47 +0300

    all except **task done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         SongHandler sh = new SongHandler();
-        //System.out.println(sh.getSong());
-        //sh.wordsCount();
-        //String s = " fucking shit";
-        //System.out.println(s.contains("fuck"));
+        System.out.println(sh.toString());
     }
 }
--- src/main/java/SongHandler.java
+++ src/main/java/SongHandler.java
@@ -1,6 +1,5 @@
 import java.io.File;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -8,8 +7,6 @@ public class SongHandler {
     private String song;
     private List<String> words;
     private List<String> exceptedWords;
-    private int goodWordCount;
-    private int badWordCount;
     private final String badWords[] = new String[]{"fuck", "shit",};
 
     public SongHandler(String song) {
@@ -19,24 +16,54 @@ public class SongHandler {
     public SongHandler() {
         File file = new File(getClass().getClassLoader().getResource("song.txt").getFile());
         this.song = FileWorker.readFile(file);
+        handleTheSong();
     }
 
     private boolean isBadWord(String word){
         for (String badWord : badWords) {
-            if(word.contains(badWord))
+            if(word.toLowerCase().contains(badWord))
                 return true;
         }
         return false;
     }
 
-    public void handle() {
+    <K,V extends Comparable<? super V>>
+    SortedSet<Map.Entry<K,V>> entriesSortedByValues(Map<K,V> map) {
+        SortedSet<Map.Entry<K,V>> sortedEntries = new TreeSet<Map.Entry<K,V>>(new WordPeriodicityComparatorDesc<K,V>());
+        sortedEntries.addAll(map.entrySet());
+        return sortedEntries;
+    }
+
+    public void handleTheSong() {
         words = Stream.of(song.split("[ ,;':.)\n(!?\\\\]+")).collect(Collectors.toList());
-        exceptedWords= words.stream().filter(e -> e.length() <= 2 && isBadWord(e)).collect(Collectors.toList());
-        badWordCount=exceptedWords.size();
-        goodWordCount=words.size()-badWordCount;
+        exceptedWords= words.stream().filter(e -> e.length() <= 2 || isBadWord(e)).collect(Collectors.toList());
+        words.removeIf(e->isBadWord(e) || e.length() <= 2);
+        System.out.println("bad:\n"+exceptedWords+"\n");
+        System.out.println("good:\n"+ words+"\n");
+    }
+
+    public List getOftenWords(int howMuchWords){
+        Map<String,Integer> wordPeriodicity = new TreeMap<>();
+        for (String word : words) {
+            if(wordPeriodicity.containsKey(word))
+                wordPeriodicity.put(word,wordPeriodicity.get(word)+1);
+            else
+                wordPeriodicity.put(word,1);
+        }
+        //System.out.println(entriesSortedByValues(wordPeriodicity));
+        //I`m waiting for your advices about how can i optimize it))
+        return entriesSortedByValues(wordPeriodicity).stream().limit(howMuchWords).collect(Collectors.toList());
     }
 
     public String getSong() {
         return song;
+    }
+
+    @Override
+    public String toString() {
+        return "Words count: "+words.size()
+                +"\nexcepted words: "+ exceptedWords.stream().distinct().collect(Collectors.toList())
+                +"\nnum of excepted words: "+exceptedWords.size()
+                +"\n5 most often words: "+getOftenWords(5);
     }
 }
